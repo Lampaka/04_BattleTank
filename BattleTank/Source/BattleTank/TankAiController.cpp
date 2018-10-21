248deb34ca5f65a2f63b05e9adc656f184216668
Lampaka <u31861@hs-harz.de>
2018-10-21 04:32:17 +0200

    BT AiTank Controller  CPP  Ai can find player and logs to console for now

---
+++ BattleTank/Source/BattleTank/TankAiController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAiController.h"
+
+void ATankAiController::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	auto PlayerTank = GetPlayerTank();
+	if (!ControlledTank && !PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Im possesed by an AI: %s"), *ControlledTank->GetName())
+			UE_LOG(LogTemp, Warning, TEXT("Target : %s"), *PlayerTank->GetName())
+	}
+}
+
+ATank* ATankAiController::GetControlledTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAiController::GetPlayerTank() const {
+
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
---
+++ BattleTank/Source/BattleTank/TankAiController.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Controller.h"
+#include "Engine/World.h"
+#include "TankAiController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAiController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+public:
+	ATank* GetControlledTank() const;
+	ATank* GetPlayerTank() const;
+	virtual void BeginPlay() override;
+
+	
+};
